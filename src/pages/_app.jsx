08572ed9837c563f5c5840ad6fe4977b4a4d6c91
Nathael Bonnal <kirua48000@gmail.com>
2023-04-13 23:57:39 +0200

    commit

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -17,24 +17,94 @@ Router.events.on('hashChangeStart', onRouteChange)
 Router.events.on('routeChangeComplete', onRouteChange)
 Router.events.on('routeChangeError', onRouteChange)
 
+function getNodeText(node) {
+  let text = ''
+  for (let child of node.children ?? []) {
+    if (typeof child === 'string') {
+      text += child
+    }
+    text += getNodeText(child)
+  }
+  return text
+}
+
+function collectHeadings(nodes, slugify = slugifyWithCounter()) {
+  let sections = []
+
+  for (let node of nodes) {
+    if (node.name === 'h2' || node.name === 'h3') {
+      let title = getNodeText(node)
+      if (title) {
+        let id = slugify(title)
+        node.attributes.id = id
+        if (node.name === 'h3') {
+          if (!sections[sections.length - 1]) {
+            throw new Error(
+              'Cannot add `h3` to table of contents without a preceding `h2`'
+            )
+          }
+          sections[sections.length - 1].children.push({
+            ...node.attributes,
+            title,
+          })
+        } else {
+          sections.push({ ...node.attributes, title, children: [] })
+        }
+      }
+    }
+
+    sections.push(...collectHeadings(node.children ?? [], slugify))
+  }
+
+  return sections
+}
+
 export default function App({ Component, pageProps }) {
   let router = useRouter()
 
+  /**
+   * Changement du path pour inclure le scope de la doc
+   */
+  if (router.pathname.startsWith('/')) {
+    let title = pageProps.markdoc?.frontmatter.title
+
+    let pageTitle =
+      pageProps.markdoc?.frontmatter.pageTitle ||
+      `${pageProps.markdoc?.frontmatter.title} - Docs`
+
+    let description = pageProps.markdoc?.frontmatter.description
+
+    let tableOfContents = pageProps.markdoc?.content
+      ? collectHeadings(pageProps.markdoc.content)
+      : []
+
+    return (
+      <>
+        <Head>
+          {router.pathname === '/' ? (
+            <title>Reforged API Reference</title>
+          ) : (
+            <title>{pageTitle}</title>
+          )}
+          {description && <meta name="description" content={description} />}
+        </Head>
+        <MDXProvider components={mdxComponents}>
+          <Layout {...pageProps} tableOfContents={tableOfContents}>
+            <Component {...pageProps} />
+          </Layout>
+        </MDXProvider>
+      </>
+    )
+  }
+
+  /**
+   * Création du router dans le LayoutHome à votre guise
+   */
   return (
     <>
-      <Head>
-        {router.pathname === '/' ? (
-          <title>Reforged API Reference</title>
-        ) : (
-          <title>{`${pageProps.title} - Reforged API Reference`}</title>
-        )}
-        <meta name="description" content={pageProps.description} />
-      </Head>
-      <MDXProvider components={mdxComponents}>
-        <Layout {...pageProps}>
-          <Component {...pageProps} />
-        </Layout>
-      </MDXProvider>
+      <LayoutHome>
+        <Component key={router.asPath} {...pageProps} />
+      </LayoutHome>
     </>
   )
 }
